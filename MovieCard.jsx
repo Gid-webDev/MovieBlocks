642245a700d01602a7bdafcecf13d5721ae7f182
Gideon <117660758+Gid-webDev@users.noreply.github.com>
2023-09-15 00:59:44 +0000

    Add files via upload
    This update addresses problems of the none functional modal, search input has been enabled and functional...

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -5,12 +5,12 @@ const images = "https://image.tmdb.org/t/p/original/"
 const MovieCard = ({Svideo}) => {
     return(
         <div className="CardsContainer">
-            <div className="Cards  image-fluid" >
+            <div className="Cards  image-fluid bg-black" >
             <img src={`${images}${Svideo.poster_path}` } alt=""/>
            <div className="py-3"> 
            <strong>
              Title: {Svideo.original_title}   {Svideo.original_name}<br/>
-             Date: {Svideo.release_date}    {Svideo.first_air_date}
+             Released: {Svideo.release_date}    {Svideo.first_air_date}
            </strong>
            </div>
           </div>
