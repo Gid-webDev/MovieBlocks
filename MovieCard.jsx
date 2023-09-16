03c56507df55b13965652dc003208e5b0d45cd97
Gideon <117660758+Gid-webDev@users.noreply.github.com>
2023-09-16 03:16:15 +0000

    Add files via upload

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -5,7 +5,7 @@ const images = "https://image.tmdb.org/t/p/original/"
 const MovieCard = ({Svideo}) => {
     return(
         <div className="CardsContainer">
-            <div className="Cards  image-fluid bg-black" >
+            <div className="Cards  image-fluid bg-black" style={{scale:'1.05', marginTop:'36px'}} >
             <img src={`${images}${Svideo.poster_path}` } alt=""/>
            <div className="py-3"> 
            <strong>
