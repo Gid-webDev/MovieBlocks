8e3119852f6a41870f69c04c5b1bbe7eaef5c70c
Gideon <117660758+Gid-webDev@users.noreply.github.com>
2023-09-20 13:27:34 +0000

    Add files via upload

---
+++ GenresContainter.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+
+const GenresContainter = ({setGenre,  GetVideo, movieOption}) => {
+    const GenresforTvSeries = [
+        {"name": "Soap",          "id":"10766"},   
+        {"name": "Talk",          "id":"10767"},
+        {"name": "Western",          "id":"37"},
+        {"name": "Animation",          "id":"16,"},
+        {"name": "Comedy",          "id":" 35"},
+        {"name": "Documentary",          "id":" 99"},
+        {"name": "Mystery",          "id":"9648"},
+        {"name": "Drama",          "id":"18"},
+        {"name": "News",          "id":"10763"},
+        {"name": "Family",          "id":" 10751"},
+        {"name": "Reality",          "id":"10764"},
+        {"name": "War & Politics",          "id":"10768"},
+        {"name": "Crime",          "id":"80"},
+        {"name": "Action & Adventure",          "id":"10759"},
+        {"name": "Kids",          "id":"10762"},
+        {"name": "Sci-Fi & Fantasy",          "id":"10765"}
+      ]
+
+      const GenresforMovies = [
+        {"name": "Action",          "id":"28"},  
+        {"name": "Comedy",          "id":"35"},
+        {"name": "Drama",           "id":"18"},  
+        {"name": "History",         "id":"36"},
+        {"name": "Mystery",         "id":"9648"},  
+        {"name": "TV Movie",        "id":"10770"},
+        {"name": "Crime",           "id":"80"},  
+        {"name": "Adventure",       "id":"12"},
+        {"name": "Documentary",     "id":"99"},
+        {"name": "Fantasy",         "id":"14"},
+        {"name": "Horror",          "id":"27"},
+        {"name": "Romance",         "id":"10749"},
+        {"name": "Science Fiction", "id":"878"},
+        {"name": "Thriller",        "id":"53"},
+        {"name": "War",             "id":"10752"},
+        {"name": "Animation",       "id":"16"},
+        {"name": "Music",           "id":"10402"},
+        {"name": "Western",         "id":"37"}
+    ]
+    const [myMovieGenre, setMyMovieGenre] = useState(GenresforMovies);
+    const [myTvGenre, setTvGenre] = useState(GenresforTvSeries);
+
+        const GetMoviesGenre = GenresforMovies.map((genreOption) =>(
+        <button key={genreOption.id} onClick={()=> GetVideo( setGenre(genreOption.id))}> 
+          {genreOption.name} 
+        </button>))
+
+        const GetTvGenre = GenresforTvSeries.map((genreOption) =>(
+            <button key={genreOption.id} onClick={()=> GetVideo( setGenre(genreOption.id))}> 
+              {genreOption.name} 
+            </button>))
+  return (
+    <div>
+      {movieOption === true? 
+        (GetMoviesGenre) : (GetTvGenre)}
+      
+    </div>
+  )
+}
+
+export default GenresContainter
